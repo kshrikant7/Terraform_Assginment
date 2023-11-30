955bfed56b4a27f53c30fb355c984ea7fa113bb9
Shrikant K <65910406+kshrikant7@users.noreply.github.com>
2023-11-30 19:36:39 +0530

    Files

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,3 @@
-
-# ec2.tf
-
-resource "aws_instance" "public_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key_pair.key_name
-  subnet_id     = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "public_instance"
 #ec2
 
 provider "aws" {
@@ -18,7 +7,7 @@ provider "aws" {
 
 resource "aws_instance" "public_instance" {
   ami                    = "ami-01b66f9994a4d8384"
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
   key_name               = aws_key_pair.ec2_key_pair.key_name
@@ -31,7 +20,7 @@ resource "aws_instance" "public_instance" {
     inline = [
       "sudo yum update -y",
       "sudo yum install -y nginx",
-      "sudo service nginx start",
+      "sudo service nginx start", 
       "sudo chkconfig nginx on",
       "sudo echo '${tls_private_key.ssh_key.private_key_pem}' > /home/ec2-user/private_key.pem",
       "sudo chmod 400 /home/ec2-user/private_key.pem"
@@ -40,7 +29,6 @@ resource "aws_instance" "public_instance" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = tls_private_key.my_private_key.private_key_pem
       private_key  = tls_private_key.ssh_key.private_key_pem
       host = self.public_ip
     }
@@ -48,41 +36,14 @@ resource "aws_instance" "public_instance" {
 }
 
 resource "aws_instance" "private_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key_pair.key_name
-  subnet_id     = aws_subnet.private_subnet.id
-
-  tags = {
-    Name = "private_instance"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      # Add any provisioning steps for private instance if needed
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = tls_private_key.my_private_key.private_key_pem
-    }
-  }
-}
-
-# Decode the SSH private key stored in Jenkins credentials
-data "tls_private_key" "my_private_key" {
-  private_key_pem = base64decode(credentials("SSH_Private"))
-}
   ami                    = "ami-01b66f9994a4d8384"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
   key_name               = aws_key_pair.ec2_key_pair.key_name
-  
-  user_data = file("user.sh")
 
   tags = {
     Name = "Private Instance"
   }
+  user_data = file("user.sh")
 }
--- user.sh
+++ user.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
 echo "Starting user data script at $(date)"
