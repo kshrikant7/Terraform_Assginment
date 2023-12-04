78e58da0c350dc31bae54f04ad2608f020055211
kshrikant7 <1999kshrikant@gmail.com>
2023-12-04 19:14:51 +0530

    Update

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,9 +1,9 @@
 # modules/ec2-instance/outputs.tf
 
 output "public_instance_id" {
-  value = aws_instance.public_instance.id
+  value = aws_instance.public_instance.public_ip
 }
 
 output "private_instance_id" {
-  value = aws_instance.private_instance.id
+  value = aws_instance.private_instance.private_ip
 }
