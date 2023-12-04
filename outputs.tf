03719d44cf7b8587a068a988ca6f10d54a75b1a8
kshrikant7 <1999kshrikant@gmail.com>
2023-12-04 19:16:56 +0530

    Update

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "public_instance_ip" {
   description = "The public IP of the public instance"
-  value       = module.ec2.public_instance_ip
+  value       = module.ec2_instance.public_instance_ip
 }
 
 output "private_instance_ip" {
   description = "The private IP of the private instance"
-  value       = module.ec2.private_instance_ip
+  value       = module.ec2_instance.private_instance_ip
 }
